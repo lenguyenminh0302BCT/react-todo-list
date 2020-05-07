a5af3fa33fc3c7d9928a96b7a05609c464145f5d
Phuc Nguyen <lenguyenminh0302@gmail.com>
2020-05-07 16:20:07 +0700

    first commit

---
+++ src/component/TodoList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Item123 from "./TodoItem";
+export default class TodoList extends Component {
+    render() {
+        return (
+            <div>
+               <Item123/>
+            </div>
+        )
+    }
+}
